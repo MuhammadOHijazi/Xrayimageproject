bd94bcc84a6b3b4ebf36fdbe0d704a16f7c7a2fd
Sami Alomar <sami.anas.alomar.2016.king@gmail.com>
2024-05-11 22:04:10 +0300

    added the import button and functionality and the selection of a part of the image

--- xrayimageproject/xrayimageproject/Form1.Designer.cs
+++ xrayimageproject/xrayimageproject/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.PaleTurquoise;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox1.Location = new Point(12, 56);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1158, 687);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkCyan;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.MenuText;
+            button1.Location = new Point(12, 15);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 35);
+            button1.TabIndex = 1;
+            button1.Text = "import";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1180, 745);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "XRayProject";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
--- xrayimageproject/xrayimageproject/Form1.cs
+++ xrayimageproject/xrayimageproject/Form1.cs
@@ -1,10 +1,93 @@
+using System.Windows.Forms;
+
 namespace xrayimageproject
 {
     public partial class Form1 : Form
     {
+        // Declare variables to track the selection
+        bool isSelecting = false;
+        Point selectionStart, selectionEnd;
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox_MouseDown);
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox_MouseMove);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox_MouseUp);
+            pictureBox1.Paint += new PaintEventHandler(pictureBox_Paint);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Start the selection from the current mouse location
+            isSelecting = true;
+            selectionStart = e.Location;
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isSelecting)
+            {
+                // Update the end point of the selection while the mouse moves
+                selectionEnd = e.Location;
+                // Redraw the pictureBox to show the selection rectangle
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isSelecting)
+            {
+                // End the selection when the mouse is up
+                isSelecting = false;
+                // to do later here, handle the selected part of the image
+
+            }
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (isSelecting)
+            {
+                // Draw a rectangle to represent the selected area
+                Rectangle rect = GetRectangle(selectionStart, selectionEnd);
+                e.Graphics.DrawRectangle(Pens.White, rect);
+            }
+        }
+
+        // Helper method to get a rectangle from two points, the first location will be the smalleset two points
+        // and the second location will be the absolute distance 
+        private Rectangle GetRectangle(Point p1, Point p2)
+        {
+            return new Rectangle(
+                Math.Min(p1.X, p2.X),
+                Math.Min(p1.Y, p2.Y),
+                Math.Abs(p1.X - p2.X),
+                Math.Abs(p1.Y - p2.Y));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
