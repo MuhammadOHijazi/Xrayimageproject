614ffeab0ad7a344b591719ea703b0af8800ad80
Dani Al-Shmmas <danialshmmas@gmail.com>
2024-05-15 12:30:05 -0400

    caption on image and report generation

--- xrayimageproject/xrayimageproject/Program.cs
+++ xrayimageproject/xrayimageproject/Program.cs
@@ -12,25 +12,25 @@ namespace xrayimageproject
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            String inputPath = "C:\\Users\\HP\\Documents\\Years To Go\\4th Year\\2nd Semester\\Multimedia\\P\\project\\D Code\\xrayimageproject\\xrayimageproject\\assets\\inputs\\xray1.jpeg";
-            string outputpath = "c:\\users\\hp\\documents\\years to go\\4th year\\2nd semester\\multimedia\\p\\project\\d code\\xrayimageproject\\xrayimageproject\\assets\\outputs\\xray1_with_caption.jpeg";
+            //String inputPath = "C:\\Users\\HP\\Documents\\Years To Go\\4th Year\\2nd Semester\\Multimedia\\P\\project\\D Code\\xrayimageproject\\xrayimageproject\\assets\\inputs\\xray1.jpeg";
+            //string outputpath = "c:\\users\\hp\\documents\\years to go\\4th year\\2nd semester\\multimedia\\p\\project\\d code\\xrayimageproject\\xrayimageproject\\assets\\outputs\\xray1_with_caption.jpeg";
 
-            // Add caption on the image
-            CaptionOnImage coi = new();
-            CaptionOnImage.SetCaption(inputPath, outputpath, "patient name");
+            //// Add caption on the image
+            //CaptionOnImage coi = new();
+            //CaptionOnImage.SetCaption(inputPath, outputpath, "patient name");
 
-            // cCreating the report end exporting as pdf
-            string name = "Abu Yani";
-            string id = "1001";
-            string info = "Has a medium tumor";
-            string imgPath = outputpath;
-            string pdfOutputPath = "C:\\Users\\HP\\Documents\\Years To Go\\4th Year\\2nd Semester\\Multimedia\\P\\project\\D Code\\xrayimageproject\\xrayimageproject\\assets\\outputs\\patient_report.pdf";
+            //// cCreating the report end exporting as pdf
+            //string name = "Abu Yani";
+            //string id = "1001";
+            //string info = "Has a medium tumor";
+            //string imgPath = outputpath;
+            //string pdfOutputPath = "C:\\Users\\HP\\Documents\\Years To Go\\4th Year\\2nd Semester\\Multimedia\\P\\project\\D Code\\xrayimageproject\\xrayimageproject\\assets\\outputs\\patient_report.pdf";
 
-            ReportGeneration repoGene = new();
-            repoGene.GenerateReport(name, id, info, imgPath, pdfOutputPath);
+            //ReportGeneration repoGene = new();
+            //repoGene.GenerateReport(name, id, info, imgPath, pdfOutputPath);
 
 
-            // Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
